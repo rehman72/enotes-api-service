3b7f9bb2b4c4c2f52bd1c2dff423f2f30c50e492
AbdulRahmanPatni <abdulrahmanpatni@gmail.com>
2025-08-16 12:54:12 +0530

    initial Commit

---
+++ src/test/java/com/project/enotes_api_service/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.enotes_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
