05d478ff76dabcba0d00cfaaf3a73678f37f2764
AbdulRahmanPatni <abdulrahmanpatni@gmail.com>
2025-08-22 16:06:43 +0530

    added Custom-Exception for Categories

--- src/main/java/com/project/enotes_api_service/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/enotes_api_service/Exception/GlobalExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.project.enotes_api_service.Exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<?> nullPointerExceptionHandler(Exception e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+
 }
