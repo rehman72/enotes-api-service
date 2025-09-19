56b23014a285fb65e977a7cfbfd83ece287c60e5
AbdulRahmanPatni <abdulrahmanpatni@gmail.com>
2025-09-19 18:41:28 +0530

    unused-import

--- src/main/java/com/project/enotes_api_service/service/UserServiceImpl.java
+++ src/main/java/com/project/enotes_api_service/service/UserServiceImpl.java
@@ -24,7 +24,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
-import javax.management.relation.RoleNotFoundException;
 import java.util.List;
 import java.util.UUID;
 
